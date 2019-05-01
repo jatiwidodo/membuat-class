12271a6c2b3140b028c47dd47696db8bb567479b
unknown <priambodo.widodo@students.amikom.ac.id>
2019-05-01 19:54:37 +0700

    membuat class

---
+++ Mahasiswa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace membuat_class
+{ 
+public class Mahasiswa
+{
+    //properties
+    public string NIM { get; set; }
+    public string Nama { get; set; }
+    public float IPK { get; set; }
+
+    //method
+    public void Registrasi()
+    {
+        Console.WriteLine("Nama : {0}", Nama);
+        Console.WriteLine("NIM  : {0}", NIM);
+        Console.WriteLine("IPK : {0}", IPK);
+        Console.WriteLine("Telah melakukan registrasi");
+    }
+
+    //method juga
+    public void IsiKRS()
+    {
+        Console.WriteLine("{0} sedang melakukan proses KRS\n", Nama);
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace membuat_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //buat objek 
+            Mahasiswa jati = new Mahasiswa();
+            Mahasiswa ratih = new Mahasiswa();
+
+            //set nilai properties
+            jati.NIM = "18.11.2243";
+            jati.Nama = "Priambodo Jati";
+            jati.IPK = 4.5f;
+
+            ratih.NIM = "18.11.2252";
+            ratih.Nama = "Ratih Nur Hasannah";
+            ratih.IPK = 5.5f;
+
+            //memanggil method
+            jati.Registrasi();
+            jati.IsiKRS();
+
+            ratih.Registrasi();
+            ratih.IsiKRS();
+
+            Console.ReadKey();
+        }
+    }
+}
